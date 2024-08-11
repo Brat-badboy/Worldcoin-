2e2e54740f104a3f13ed1461d3c1fae560674949
Brat-badboy <joackimmagero@gmail.com>
2024-08-11 08:39:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log('Hello World!');
+
+document.getElementById("digitaldollarBtn").onclick = function() {
+    setTimeout(function() {
+        window.location.href = "digitaldollars.html";
+    }, 500);
+};
+document.getElementById("cryptoBtn").onclick = function() {
+    setTimeout(function() {
+        window.location.href = "crypto.html";
+    }, 500);
+};
+document.getElementById("worldcoinBtn").onclick = function() {
+    setTimeout(function() {
+        window.location.href = "worldcoin.html";
+    }, 500);
+};
+document.getElementById("sellBtn").onclick = function() {
+  setTimeout(function() {
+    window.location.href = "deposit.html";
+  }, 500);
+};
+document.getElementById("buyBtn").onclick = function() {
+  setTimeout(function() {
+    window.location.href = "deposit.html";
+  }, 500);
+};
+
